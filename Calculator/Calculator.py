44816bceb8ed89e38ab7a97349cab7a9309ec1ef
Invizabel <46697708+Invizabel@users.noreply.github.com>
2021-10-22 10:05:31 -0400

    Add files via upload

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -7,7 +7,18 @@ def cube(x):
     return x * x * x
 
 def division(x, y):
-    return x / y
+    solution = 0
+    
+    if y == 0:
+        solution = "undefined"
+
+    else:
+        solution = x / y
+
+    return solution
+
+def exponent(x, y):
+    return x ** y
 
 def multiplication(x, y):
     return x * y
@@ -23,7 +34,7 @@ def subtraction(x, y):
 
 while True:
     os.system("clear")
-    user_input = input("What do you want to do?\n0- exit\n1- addition\n2- cube\n3- division\n4- multiplication\n5- square\n6- square root\n7- subtraction\n")
+    user_input = input("What do you want to do?\n0- exit\n1- addition\n2- cube\n3- division\n4- exponent\n5- multiplication\n6- square\n7- square root\n8- subtraction\n")
 
     if user_input == "0":
         exit()
@@ -47,21 +58,27 @@ while True:
 
     if user_input == "4":
         os.system("clear")
+        number_1 = int(input("Base number: "))
+        number_2 = int(input("Power number: "))
+        print(exponent(number_1, number_2))
+
+    if user_input == "5":
+        os.system("clear")
         number_1 = int(input("Number 1: "))
         number_2 = int(input("Number 2: "))
         print(multiplication(number_1, number_2))
 
-    if user_input == "5":
+    if user_input == "6":
         os.system("clear")
         number = int(input("Number: "))
         print(square(number))
 
-    if user_input == "6":
+    if user_input == "7":
         os.system("clear")
         number = int(input("Number: "))
         print(square_root(number))
 
-    if user_input == "7":
+    if user_input == "8":
         os.system("clear")
         number_1 = int(input("Number 1: "))
         number_2 = int(input("Number 2: "))
