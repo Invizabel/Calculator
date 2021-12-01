c7eb74a59bb91aeac126f001ae11ef13fb792697
Invizabel <46697708+Invizabel@users.noreply.github.com>
2021-12-01 06:38:46 -0600

    Add files via upload

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -1,10 +1,11 @@
-import os
-
 def addition(x, y):
     return x + y
 
 def cube(x):
     return x * x * x
+
+def cube_root(x):
+    return x ** (1/3)
 
 def division(x, y):
     solution = 0
@@ -31,63 +32,3 @@ def square_root(x):
 
 def subtraction(x, y):
     return x - y
-
-while True:
-    os.system("clear")
-    user_input = input("What do you want to do?\n0- exit\n1- addition\n2- cube\n3- division\n4- exponent\n5- multiplication\n6- square\n7- square root\n8- subtraction\n")
-
-    if user_input == "0":
-        exit()
-
-    if user_input == "1":
-        os.system("clear")
-        number_1 = float(input("Number 1: "))
-        number_2 = float(input("Number 2: "))
-        print(addition(number_1, number_2))
-        pause = input()
-
-    if user_input == "2":
-        os.system("clear")
-        number = float(input("Number: "))
-        print(cube(number))
-        pause = input()
-
-    if user_input == "3":
-        os.system("clear")
-        number_1 = float(input("Number 1: "))
-        number_2 = float(input("Number 2: "))
-        print(division(number_1, number_2))
-        pause = input()
-
-    if user_input == "4":
-        os.system("clear")
-        number_1 = float(input("Base number: "))
-        number_2 = float(input("Power number: "))
-        print(exponent(number_1, number_2))
-        pause = input()
-
-    if user_input == "5":
-        os.system("clear")
-        number_1 = float(input("Number 1: "))
-        number_2 = float(input("Number 2: "))
-        print(multiplication(number_1, number_2))
-        pause = input()
-
-    if user_input == "6":
-        os.system("clear")
-        number = float(input("Number: "))
-        print(square(number))
-        pause = input()
-
-    if user_input == "7":
-        os.system("clear")
-        number = float(input("Number: "))
-        print(square_root(number))
-        pause = input()
-
-    if user_input == "8":
-        os.system("clear")
-        number_1 = float(input("Number 1: "))
-        number_2 = float(input("Number 2: "))
-        print(subtraction(number_1, number_2))
-        pause = input()
